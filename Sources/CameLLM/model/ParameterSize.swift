7cd0e42834f58a46f15eb2d162562f7ebc40ac61
Alex Rozanski <alex@rozanski.me>
2023-05-09 15:17:09 +0100

    add helper to initialize ParameterSize from String

--- Sources/CameLLM/model/ParameterSize.swift
+++ Sources/CameLLM/model/ParameterSize.swift
@@ -11,3 +11,20 @@ public enum ParameterSize: Codable, Equatable {
   case millions(Decimal)
   case billions(Decimal)
 }
+
+public extension ParameterSize {
+  static func from(string: String) -> ParameterSize? {
+    let scanner = Scanner(string: string.trimmingCharacters(in: .whitespacesAndNewlines))
+    guard let decimal = scanner.scanDecimal() else {
+      return nil
+    }
+
+    if scanner.scanString("B") != nil {
+      return .billions(decimal)
+    } else if scanner.scanString("M") != nil {
+      return .millions(decimal)
+    }
+
+    return nil
+  }
+}
